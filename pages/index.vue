0d68fdc1bd5a8444abdd067436c40d18060e978d
Hasan Misbah <hasanmisbah01@gmail.com>
2020-01-19 18:37:49 +0600

    Fixed skill c ard issues

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
           <div class="column intro-content">
             <p>Hello! I am</p>
             <h1 class="title is-1">Hasan Misbah</h1>
-            <p>I'm a frontend Developer, WordPress Fanatic &amp; Linux Freak</p>
+            <p>frontend Developer, WordPress Fanatic &amp; Linux Freak</p>
             <div class="action-btn">
               <a href="#" class="btn is-primary">Get Resume</a>
               <a href="#" class="btn outline is-primary">About Me</a>
@@ -27,6 +27,7 @@
                 I am
                 <strong>Hasan Misbah</strong>. From Sylhet, Bangladesh. I’m a front-end developer
               </p>
+              <p>I am enthusiastic about designing and developing user friendly interface. I always learn new concepts and ideas to stay up to date with new technology.</p>
               <p>I create fun, innovative, accessible, and fast websites. I try to write every bit I code I touch more readable, modular, performant and accessible than I found it.</p>
               <p>
                 Beyond front-end coding chops, I’m also passionate about sharing what I’ve learned both with my teammates and the broader web community.
@@ -40,19 +41,22 @@
           </div>
           <div class="column about-content is-one-third-desktop">
             <div class="about-content">
-              <img class="image" src="../assets/img/hasanmisbah.jpg" alt="Hasan Misbah" />
+              <div class="images">
+                <img class="image" src="../assets/img/hasanmisbah.jpg" alt="Hasan Misbah" />
+              </div>
             </div>
           </div>
         </div>
       </div>
     </section>
-    <section id="skill">
-      <div class="container is-full-height">
-        <div class="columns">
+    <section id="skill" class="section is-full-height">
+      <div class="container">
+        <div class="columns is-desktop is-vcentered is-vertical-center">
           <div class="column">
             <div class="section-title">
               <h3 class="title is-3">About Me</h3>
             </div>
+
             <div class="skills">
               <div class="card">
                 <h4>React</h4>
